f0689dcb46393c7a4d4c16d118dc8e8dc9ee7e3c
josia <josiahmedina0318@gmail.com>
2023-11-09 18:40:53 -0700

    changed code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -1,41 +1,52 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 @TeleOp
-public class MecanumTeleOp extends OpMode{
-
-    DcMotor RFMotor;
-    DcMotor LFMotor;
-    DcMotor RBMotor;
-    DcMotor LBMotor;
-
-    public void moveDriveTrain(){
-        double vertical = 0;
-        double horizontal = 0;
-        double pivot = 0;
-
-        vertical = -gamepad1.left_stick_y;
-        horizontal = gamepad1.left_stick_x;
-        pivot = gamepad1.right_stick_x;
-    }
-
+public class MecanumTeleOp extends LinearOpMode {
     @Override
-    public void init() {
-        RFMotor = hardwareMap.get(DcMotor.class,"RFMotor" );
-        LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
-        RBMotor = hardwareMap.get(DcMotor.class, "RBMotor");
-        LBMotor = hardwareMap.get(DcMotor.class, "LBMotor");
+    public void runOpMode() throws InterruptedException {
+        DcMotor LFMotor = hardwareMap.dcMotor.get("LFMotor");
+        DcMotor LBMotor = hardwareMap.dcMotor.get("LBMotor");
+        DcMotor RFMotor = hardwareMap.dcMotor.get("RFMotor");
+        DcMotor RBMotor = hardwareMap.dcMotor.get("RBMotor");
+
+        // Reverse the right side motors.
+        // If your robot moves backwards when commanded to go forwards, reverse the left side instead.
 
         RFMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         RBMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-    }
+        DcMotor IntakeMotor = hardwareMap.dcMotor.get("IntakeMotor");
 
-    @Override
-    public void loop() {
+        waitForStart();
 
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+            boolean dpadUp = gamepad1.dpad_up;
+            boolean dpadDown = gamepad1.dpad_down;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            LFMotor.setPower(frontLeftPower);
+            LBMotor.setPower(backLeftPower);
+            RFMotor.setPower(frontRightPower);
+            RBMotor.setPower(backRightPower);
+
+        }
     }
 }
