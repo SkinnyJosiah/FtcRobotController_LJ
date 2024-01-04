7fce276100264be334986488ca3c1adeeb4a6dd9
SkinnyJosiah <94023705+SkinnyJosiah@users.noreply.github.com>
2024-01-04 08:55:11 -0700

    Update Autonomous.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -3,9 +3,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous
-public class Autonomous extends LinearOpMode {
+public class RedAutonomous extends LinearOpMode {
 
     protected DcMotor LFMotor;
     protected DcMotor LBMotor;
@@ -14,27 +15,39 @@ public class Autonomous extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException{
+
+        /*
+        Define motors and servos
+         */
+
         LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
         LBMotor = hardwareMap.get(DcMotor.class, "LBMotor");
         RFMotor = hardwareMap.get(DcMotor.class, "RFMotor");
         RBMotor = hardwareMap.get(DcMotor.class, "RBMotor");
 
-        // LF MOTOR
+        Servo ServoTilt = hardwareMap.servo.get("ServoTilt");
+        Servo ServoLeftClaw = hardwareMap.servo.get("ServoLeftClaw");
+        Servo ServoRightClaw = hardwareMap.servo.get("ServoRightClaw");
+
+        // LF MOTOR (reset position upon each run)
         LFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         LFMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        // LB MOTOR
+
+        // LB MOTOR(reset position upon each run)
         LBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         LBMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        // RF MOTOR
+
+        // RF MOTOR(reset position upon each run)
         RFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         RFMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        // RB MOTOR
+
+        // RB MOTOR(reset position upon each run)
         RBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         RBMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-
         // REVERSE RIGHT SIDE MOTORS (very important, otherwise robot is erratic)
         // If robot moves backwards when commanded to go forwards, reverse the left side instead.
+
         RFMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         RBMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
@@ -43,7 +56,8 @@ public class Autonomous extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-            // These are the motors' positions.
+
+            // These are the motors' positions, shown on the Driver Hub.
             double LFPosition = LFMotor.getCurrentPosition();
             double LBPosition = LBMotor.getCurrentPosition();
             double RFPosition = RFMotor.getCurrentPosition();
@@ -55,6 +69,37 @@ public class Autonomous extends LinearOpMode {
             telemetry.addData("Right Front Motor Position", RFPosition);
             telemetry.addData("Right Back Motor Position", RBPosition);
             telemetry.update();
+
+            // Move forward. Push purple pixel onto the red tape.
+            LFMotor.setPower(1);
+            LBMotor.setPower(1);
+            RFMotor.setPower(1);
+            RBMotor.setPower(1);
+
+            sleep (500);
+
+            // Robot moves backward, to wall.
+            LFMotor.setPower(-1);
+            LBMotor.setPower(-1);
+            RFMotor.setPower(-1);
+            RBMotor.setPower(-1);
+
+            sleep(500);
+
+            // Robot goes to the right to park.
+            LFMotor.setPower(1);
+            LBMotor.setPower(-1);
+            RFMotor.setPower(-1);
+            RBMotor.setPower(1);
+
+            sleep (1000);
+
+            // Stop when parked.
+            LFMotor.setPower(0);
+            LBMotor.setPower(0);
+            RFMotor.setPower(0);
+            RBMotor.setPower(0);
+
         }
     }
 }
