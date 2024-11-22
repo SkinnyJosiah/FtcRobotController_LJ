821a24e16d3ba501a6a0fe05359ab1365aaede4f
Levi Weyna <145080653+levyweyna808@users.noreply.github.com>
2024-11-21 22:38:44 -0700

    AXON BABYYYYYYYYYYYYYYYYY

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SM_test_OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SM_test_OpMode.java
@@ -10,6 +10,7 @@ import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.sfdev.assembly.state.StateMachine;
 import com.sfdev.assembly.state.*;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
@@ -21,6 +22,12 @@ public class SM_test_OpMode extends LinearOpMode {
         DEPOSITING,
         RETRACTING
     }
+    private ElapsedTime runtime = new ElapsedTime();
+    static final double COUNTS_PER_MOTOR_BILDA = 28;
+    static final double LINEAR_SLIDE_GEARING = 	19.20;
+    static final double PULLEY_WHEEL_DIAMETER_INCHES = 1.45;
+    static final double COUNTS_PER_LS_INCH = (COUNTS_PER_MOTOR_BILDA / LINEAR_SLIDE_GEARING) * (PULLEY_WHEEL_DIAMETER_INCHES * 3.1415 * 2);
+    static final double LINEAR_SLIDE_SPEED = 1.0;
     @Override
     public void runOpMode() throws InterruptedException {
 
@@ -29,13 +36,12 @@ public class SM_test_OpMode extends LinearOpMode {
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("LBMotor");
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("RFMotor");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("RBMotor");
-
         // Misc. Motors
         DcMotor intakeMotor = hardwareMap.dcMotor.get("intakeMotor");
         DcMotor horizontalSlideMotor = hardwareMap.dcMotor.get("horizontalSlideMotor");
         DcMotor leftSlideMotor = hardwareMap.dcMotor.get("leftSlideMotor");
         DcMotor rightSlideMotor = hardwareMap.dcMotor.get("rightSlideMotor");
-
+        //encoders
         // Reverse Motor direction for proper driving
         frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -48,13 +54,21 @@ public class SM_test_OpMode extends LinearOpMode {
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftSlideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightSlideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //encoders
+        leftSlideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightSlideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftSlideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightSlideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         // Servos
         Servo leftIntake = hardwareMap.servo.get("leftIntake");
         Servo rightIntake = hardwareMap.servo.get("rightIntake");
         Servo spoolServo = hardwareMap.servo.get("spoolServo");
         Servo meshNet = hardwareMap.servo.get("meshNet");
+        Servo horSlideLeft = hardwareMap.servo.get("horSlideLeft");
+        Servo horSlideRight = hardwareMap.servo.get("horSlideRight");
 
-        StateMachine machine = new StateMachineBuilder()
+        //position stuff
+        /*StateMachine machine = new StateMachineBuilder()
                 .state(IntakeAndOuttake.IDLING)
                 .onEnter( () -> {
                     rightSlideMotor.setPower(0.3);
@@ -70,18 +84,52 @@ public class SM_test_OpMode extends LinearOpMode {
                 //.transition( () -> gamepad1.b) // if check2 is false transition
                 //.state(IntakeAndOuttake.DEPOSITING)
                 //.onEnter( () -> System.out.println( "In the third state " ) )
-                .build();
+                .build();*/
 
         waitForStart();
+        //machine.start();
+        /*public void LinearSlideDrive(double slideSpeed, double slideRotationInches, double slideTmeoutS){
+            int rightSlideTarget;
+            int leftSlideTarget;
+            // Ensure that the opmode is still active
+            if (opModeIsActive()) {
 
-        machine.start();
+                // Determine new target position, and pass to motor controller
+                rightSlideTarget = rightSlideMotor.getCurrentPosition() + (int)(slideRotationInches * COUNTS_PER_LS_INCH);
+                leftSlideTarget = leftSlideMotor.getCurrentPosition() + (int)(slideRotationInches * COUNTS_PER_LS_INCH);
 
+                rightSlideMotor.setTargetPosition(rightSlideTarget);
+                leftSlideMotor.setTargetPosition(leftSlideTarget);
+                // Turn On RUN_TO_POSITION
+                rightSlideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                leftSlideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                // reset the timeout time and start motion.
+                runtime.reset();
+                rightSlideMotor.setPower(Math.abs(slideSpeed));
+                leftSlideMotor.setPower(Math.abs(slideSpeed));
+
+                while (opModeIsActive() && (runtime.seconds() < slideTmeoutS) && (leftSlideMotor.isBusy() && (rightSlideMotor.isBusy()))) {
+                    telemetry.update();
+                }
+                // Stop all motion;
+                rightSlideMotor.setPower(0);
+                leftSlideMotor.setPower(0);
+
+                // Turn off RUN_TO_POSITION
+                rightSlideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                leftSlideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+                sleep(250);   // optional pause after each move.
+            }
+        }*/
         if (isStopRequested()) return;
 
         double powerMultiplier = 1.0; // Start at full power
         boolean isHalfPower = false; // Track power state
 
         while (opModeIsActive()) {
+            //machine.update();
             double y = -gamepad1.left_stick_y;
             double x = -gamepad1.right_stick_x;
             double rx = -gamepad1.left_stick_x;
@@ -102,13 +150,6 @@ public class SM_test_OpMode extends LinearOpMode {
                 isHalfPower = false; // Reset
             }
 
-            // if(gamepad2.dpad_left) {
-            //horizontalSlideMotor.setPower(-0.3);
-            //}
-
-            //if(gamepad2.dpad_right) {
-            //horizontalSlideMotor.setPower(0.3);
-            //}
             if (gamepad2.right_bumper) {
                 powerMultiplier = 1.0; // Set to full powa!
             }
@@ -133,6 +174,15 @@ public class SM_test_OpMode extends LinearOpMode {
             else if(gamepad2.b){
                 rightIntake.setPosition(1);
                 leftIntake.setPosition(0);
+            }
+
+            if (gamepad1.circle){
+                horSlideLeft.setPosition(-0.295);
+                horSlideRight.setPosition(-0.295);
+            }
+            else if (gamepad1.cross) {
+                horSlideLeft.setPosition(0.06);
+                horSlideRight.setPosition(0.06);
             }
             double slidePowerUp = gamepad2.right_trigger;  // Get the right trigger value (0.0 to 1.0)
             double slidePowerDown = gamepad2.left_trigger; // Get the left trigger value (0.0 to 1.0)
