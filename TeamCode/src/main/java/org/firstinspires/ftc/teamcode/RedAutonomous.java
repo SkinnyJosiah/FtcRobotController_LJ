c60201e0a89af49ecea5df7c8e65e516b6fa5f30
SkinnyJosiah <94023705+SkinnyJosiah@users.noreply.github.com>
2024-12-30 11:47:57 -0700

    Update RedAutonomous.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutonomous.java
@@ -9,20 +9,6 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvPipeline;
-import org.openftc.easyopencv.OpenCvWebcam;
-
-import javax.lang.model.element.VariableElement;
-
 @Autonomous
 public class RedAutonomous extends LinearOpMode {
 
@@ -43,14 +29,10 @@ public class RedAutonomous extends LinearOpMode {
     // Unused Voltage Sensor
     protected VoltageSensor VoltageSensor;
 
-    //Camera
-    OpenCvWebcam webcam;
-
     @Override
     public void runOpMode() throws InterruptedException {
 
-        //Redefine and make motors work.
-
+        // Redefine and make motors work.
         LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
         LBMotor = hardwareMap.get(DcMotor.class, "LBMotor");
         RFMotor = hardwareMap.get(DcMotor.class, "RFMotor");
@@ -73,7 +55,6 @@ public class RedAutonomous extends LinearOpMode {
         Servo ServoRightClaw = hardwareMap.servo.get("ServoRightClaw");
 
         // Reset all positions back to 0 after every use.
-
         LFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         LFMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
@@ -86,31 +67,10 @@ public class RedAutonomous extends LinearOpMode {
         RBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         RBMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        // Initialize camera and set up OpenCV pipeline
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
-        webcam.setPipeline(new SamplePipeline());
-        webcam.setMillisecondsPermissionTimeout(5000);
-        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
-            @Override
-            public void onOpened() {
-                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
-            }
-
-            @Override
-            public void onError(int errorCode) {
-                /*
-                 * This will be called if the camera could not be opened
-                 */
-            }
-        });
-
         waitForStart();
 
         while (opModeIsActive()) {
-
-            // Add motor telemetry data (currently not working)
-
+            // Add motor telemetry data
             double LFPosition = LFMotor.getCurrentPosition();
             double LBPosition = LBMotor.getCurrentPosition();
             double RFPosition = RFMotor.getCurrentPosition();
@@ -122,12 +82,7 @@ public class RedAutonomous extends LinearOpMode {
             telemetry.addData("Right Back Motor Position", RBPosition);
             telemetry.update();
 
-            // Calculate the scaling factor based off of what previously worked. Unused.
-            //double scalingFactor = 0.275 / 12.28;
-
             // Power based off of voltage and scale
-            // This is almost always changing. Refer to https://www.desmos.com/calculator/gyl9bsxfqc
-
             double power = 0.28;
 
             // Moving stuff!!
@@ -149,16 +104,7 @@ public class RedAutonomous extends LinearOpMode {
             telemetry.update();
 
             while (opModeIsActive()) {
-                telemetry.addData("Frame Count", webcam.getFrameCount());
-                telemetry.addData("FPS", String.format("%.2f", webcam.getFps()));
-                telemetry.addData("Pipeline time ms", webcam.getPipelineTimeMs());
                 telemetry.update();
-
-                if (gamepad1.left_stick_button) {
-                    webcam.stopStreaming();
-                    webcam.closeCameraDevice();
-                }
-
                 sleep(100);
             }
 
@@ -166,8 +112,7 @@ public class RedAutonomous extends LinearOpMode {
         }
     }
 
-    // Functions for moving. Self explanatory, just call upon the function and give the first parameter power. motionTime will be removed soon once we have odometry pods and better encoder usage.
-
+    // Functions for moving
     private void moveForward(double power, long motionTime) {
         LFMotor.setPower(-power);
         LBMotor.setPower(power);
@@ -214,162 +159,5 @@ public class RedAutonomous extends LinearOpMode {
         RFMotor.setPower(power);
         RBMotor.setPower(0);
         sleep(motionTime);
-    }
-
-    // A LOT of camera work.
-
-    class SamplePipeline extends OpenCvPipeline {
-        boolean viewportPaused;
-        ColorDetection colorDetection;
-
-        public SamplePipeline() {
-            colorDetection = new ColorDetection("red");
-        }
-
-        @Override
-        public Mat processFrame(Mat input) {
-            // Process the frame using color detection and saturation check
-            Mat processedFrame = colorDetection.processFrame(input);
-
-            // Draw rectangles on the original input frame
-            drawRedRectangles(input);
-
-            return processedFrame;
-        }
-
-        private void drawRedRectangles(Mat input) {
-            // Center Rectangle
-            Imgproc.rectangle(
-                    input,
-                    new Point(input.cols() / 4, input.rows() / 8),
-                    new Point(input.cols() * (2f / 4f), input.rows() * (2f / 3f)),
-                    new Scalar(128, 0, 0), 2); // Draw the rectangle outline in red
-
-            // Right Rectangle
-            Imgproc.rectangle(
-                    input,
-                    new Point(input.cols() * (3f / 4f), input.rows() / 8),
-                    new Point(input.cols() - 1, input.rows() * (3f / 4f)),
-                    new Scalar(128, 0, 0), 2); // Draw the rectangle outline in red
-        }
-
-
-        @Override
-        public void onViewportTapped() {
-            viewportPaused = !viewportPaused;
-
-            if (viewportPaused) {
-                webcam.pauseViewport();
-            } else {
-                webcam.resumeViewport();
-            }
-        }
-    }
-
-    public static class ColorDetection extends OpenCvPipeline {
-        Mat hsv = new Mat();
-        Mat mask1, mask2 = new Mat();
-        Mat end = new Mat();
-
-        String color;
-
-        public ColorDetection(String color) {
-            this.color = color;
-        }
-
-        @Override
-        public Mat processFrame(Mat input) {
-            Scalar scalarLow, scalarHigh;
-
-            if (color.equals("blue")) {
-                // Blue color range
-                scalarLow = new Scalar(90, 100, 100);
-                scalarHigh = new Scalar(140, 255, 255);
-            } else if (color.equals("red")) {
-                // Red color range
-                scalarLow = new Scalar(0, 70, 50);
-                scalarHigh = new Scalar(8, 255, 255);
-
-                Imgproc.cvtColor(input, hsv, Imgproc.COLOR_RGB2HSV);
-                mask1 = new Mat();
-                mask2 = new Mat();
-                Core.inRange(hsv, new Scalar(172, 70, 50), new Scalar(180, 255, 255), mask1);
-                Core.inRange(hsv, new Scalar(0, 70, 50), new Scalar(8, 255, 255), mask2);
-                Core.bitwise_or(mask1, mask2, end);
-            } else {
-                // Default color range
-                scalarLow = new Scalar(100, 0, 0);
-                scalarHigh = new Scalar(0, 0, 100);
-            }
-
-            // Check saturation and change color accordingly
-            Imgproc.cvtColor(input, hsv, Imgproc.COLOR_RGB2HSV);
-            Core.inRange(hsv, scalarLow, scalarHigh, end);
-            checkAndChangeColor(input, end, 30);
-
-            return input;
-        }
-
-        private void checkAndChangeColor(Mat input, Mat mask, int saturationThreshold) {
-            Mat temp = new Mat();
-            Imgproc.cvtColor(input, temp, Imgproc.COLOR_RGB2HSV);
-
-            // Counters for red and total pixels in each rectangle
-            int redPixelCountCenter = 0;
-            int totalPixelCountCenter = 0;
-
-            int redPixelCountRight = 0;
-            int totalPixelCountRight = 0;
-
-            for (int i = 0; i < mask.rows(); i++) {
-                for (int j = 0; j < mask.cols(); j++) {
-                    double[] pixel = mask.get(i, j);
-                    if (pixel[0] > 0) {
-                        double saturation = temp.get(i, j)[1];
-
-                        // Check if the pixel is within the center rectangle
-                        if (j >= input.cols() / 4 && j <= input.cols() * (2f / 4f) &&
-                                i >= input.rows() / 8 && i <= input.rows() * (2f / 3f)) {
-                            totalPixelCountCenter++;
-
-                            if (saturation >= saturationThreshold) {
-                                redPixelCountCenter++;
-                            }
-                        }
-
-                        // Check if the pixel is within the right rectangle
-                        if (j >= input.cols() * (3f / 4f) && j <= input.cols() - 1 &&
-                                i >= input.rows() / 8 && i <= input.rows() * (3f / 4f)) {
-                            totalPixelCountRight++;
-
-                            if (saturation >= saturationThreshold) {
-                                redPixelCountRight++;
-                            }
-                        }
-                    }
-                }
-            }
-
-            // Calculate the percentage of red pixels in each rectangle
-            double percentageRedCenter = (double) redPixelCountCenter / totalPixelCountCenter;
-            double percentageRedRight = (double) redPixelCountRight / totalPixelCountRight;
-
-            // Check if the percentage of red pixels is greater than 40%
-            if (percentageRedCenter > 0.75) {
-                Imgproc.rectangle(
-                        input,
-                        new Point(input.cols() / 4, input.rows() / 8),
-                        new Point(input.cols() * (2f / 4f), input.rows() * (2f / 3f)),
-                        new Scalar(0, 255, 0), -1); // Turn the rectangle green
-            }
-
-            if (percentageRedRight > 0.75) {
-                Imgproc.rectangle(
-                        input,
-                        new Point(input.cols() * (3f / 4f), input.rows() / 8),
-                        new Point(input.cols() - 1, input.rows() * (3f / 4f)),
-                        new Scalar(0, 255, 0), -1); // Turn the rectangle green
-            }
-        }
     }
 }
